17d5b399e497c0dd612b2a0766e484e1c3fb8aa3
isopho01 <isopho01@cs.ucy.ac.cy>
2019-11-16 20:53:51 +0200

    Added image in between carousel and next component

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-
 // Re-usable components
 import CarouselHp           from '../Common/CarouselHP';
+// Images
+import logo_img             from '../../logo.svg';
 
 class Home extends Component {
     render() {
         return (
-            <CarouselHp />
+            <div>
+                <CarouselHp />
+
+                <div id = "author">
+                    <img src = { logo_img }  alt={'author'}/>
+                </div>
+            </div>
         )
     }
 }
