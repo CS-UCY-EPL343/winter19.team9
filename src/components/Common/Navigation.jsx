49ff1817eb5ea2267dcbb5dcd66a907589d9de2e
isopho01 <isopho01@cs.ucy.ac.cy>
2019-11-22 14:32:14 +0200

    Generalized ToggleModal Component to be used for other Modals

--- src/components/Common/Navigation.jsx
+++ src/components/Common/Navigation.jsx
@@ -1,47 +1,54 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
-import ToggleModal from "./ToggleModal";
+import React, { Component } from 'react';
+import { Link }             from 'react-router-dom';
+import ToggleModal          from './ToggleModal';
+import LoginModal           from './LoginModal';
 
 class Navigation extends Component {
     render() {
         return (
             // Navigation
-            <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
-                <div className="container-fluid mx-auto">
-                    <Link className="navbar-brand" to="/">
+            <nav className = "navbar navbar-expand-lg navbar-dark fixed-top" id = "mainNav">
+                <div className = "container-fluid mx-auto">
+                    <Link className = "navbar-brand" to = "/">
                         Fitness Factory Nicosia&nbsp;
-                        <img id={'logo'} src={this.props.img} alt='Logo' height={48} width={48}/>
+                        <img id = { 'logo' } src = { this.props.img } alt = 'Logo' height = { 48 } width = { 48 } />
                     </Link>
-                    <button className="navbar-toggler navbar-toggler-right"
-                            type="button"
-                            data-toggle="collapse"
-                            data-target="#navbarResponsive"
-                            aria-controls="navbarResponsive"
-                            aria-expanded="false"
-                            aria-label="Toggle navigation"
+                    <button className = "navbar-toggler navbar-toggler-right"
+                            type = "button"
+                            data-toggle = "collapse"
+                            data-target = "#navbarResponsive"
+                            aria-controls = "navbarResponsive"
+                            aria-expanded = "false"
+                            aria-label = "Toggle navigation"
                     >
                         Menu
-                        <i className="fas fa-bars"/>
+                        <i className = "fas fa-bars" />
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarResponsive">
-                        <ul className="navbar-nav text-uppercase ml-auto">
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/">Services</Link>
+                    <div className = "collapse navbar-collapse" id = "navbarResponsive">
+                        <ul className = "navbar-nav text-uppercase ml-auto">
+                            <li className = "nav-item">
+                                <Link className = "nav-link" to = "/">Services</Link>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/">Classes</Link>
+                            <li className = "nav-item">
+                                <Link className = "nav-link" to = "/">Classes</Link>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/about">About</Link>
+                            <li className = "nav-item">
+                                <Link className = "nav-link" to = "/about">About</Link>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/">Team</Link>
+                            <li className = "nav-item">
+                                <Link className = "nav-link" to = "/">Team</Link>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/">Contact&nbsp;Us</Link>
+                            <li className = "nav-item">
+                                <Link className = "nav-link" to = "/">Contact&nbsp;Us</Link>
                             </li>
-                            <li className="nav-item">
-                                <ToggleModal/>
+                            <li className = "nav-item">
+                                <ToggleModal
+                                    btnClass = { 'nav-link' }
+                                    btnText = { ['Login/Register ', <i className = "fas fa-sign-in-alt" />] }
+                                    modalSize = { 'md' }
+                                    modalHeader = { 'Login Form' }
+                                    modalBody={<LoginModal/>}
+                                />
                             </li>
                         </ul>
                     </div>
--- src/components/Common/ToggleModal.jsx
+++ src/components/Common/ToggleModal.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import {Button, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
-import LoginModal from "./LoginModal";
+import React                                                  from 'react';
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 
 export default class ToggleModal extends React.Component {
     constructor(props) {
@@ -13,29 +12,29 @@ export default class ToggleModal extends React.Component {
 
     toggle = () => {
         this.setState({
-            modal: !this.state.modal
-        });
+                          modal: !this.state.modal
+                      });
     };
 
     render() {
+        let Comp = this.props.modalBody;
         return (
             <div>
-                {/*<Button color="danger" onClick={this.toggle}>Open</Button>*/}
-                <Button className="nav-link" onClick={this.toggle}
-                >Login/Register <i className="fas fa-sign-in-alt"/></Button>
+                {/*<Button color="danger" onClick={this.toggle}>Open</Button>*/ }
+                <Button className = { this.props.btnClass } onClick = { this.toggle }
+                >{ this.props.btnText || 'Button' }</Button>
 
-                <Modal isOpen={this.state.modal} toggle={this.toggle}
-                       size="md"
-                       aria-labelledby="contained-modal-title-vcenter"
-                       centered>
-                    <ModalHeader toggle={this.toggle}>Login Form</ModalHeader>
+                <Modal isOpen = { this.state.modal } toggle = { this.toggle }
+                       size = { this.props.modalSize || 'lg' }
+                       aria-labelledby = "contained-modal-title-vcenter"
+                       centered
+                >
+                    <ModalHeader toggle = { this.toggle }>{ this.props.modalHeader }</ModalHeader>
                     <ModalBody>
-                        <LoginModal
-                            toggle={this.toggle}
-                        />
+                        { React.cloneElement(Comp, { toggle: this.toggle }) }
                     </ModalBody>
                     <ModalFooter>
-                        <Button onClick={this.toggle}>Cancel</Button>
+                        <Button onClick = { this.toggle }>{ this.props.btnCancel || 'Cancel' }</Button>
                     </ModalFooter>
                 </Modal>
             </div>
