d41e8f4ac101568714d623019c2a463234729234
aparas01 <andreas@prskv.com>
2020-03-17 16:35:23 +0200

    fixes

--- api/database.js
+++ api/database.js
@@ -16,7 +16,7 @@ function dbConnect() {
         }
         console.log('\x1b[32m%s\x1b[0m', 'Connected to database.');
         connection = conn;
-    }).catch(err);
+    });
     conn.on('error', function() {
         dbDisconnect();
         return null;
@@ -226,16 +226,6 @@ function enrollUser(CLASS_ID, User_ID) {
     });
 }
 
-// //for profile pic
-// function getProfilePic(name) {
-//     return new Promise((resolve, reject) => {
-//         const sql = "SELECT * FROM ACCOUNT a, USERS u WHERE u.name = ? and u.User_ID = a.User_ID";
-//         connection.query(sql, [name], function (err, rows) {
-//             if (err) reject(err);
-//             resolve(rows);
-//         });
-//     });
-// }
 
 //mine
 function getUserInfo(name) {
@@ -430,5 +420,5 @@ module.exports = {
     getCoaches,
     createNewMessage,
     updateAnnouncement,
-    getProfilePic
+
 };
