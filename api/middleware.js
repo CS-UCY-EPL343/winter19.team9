90eea9adc96b3310b1c08e5c96f03f2f9fa01b06
isopho01 <isopho01@cs.ucy.ac.cy>
2020-03-20 23:14:39 +0200

    Fixed Database Issue & Dashboard progress (Staff, Leaderboards)

--- api/middleware.js
+++ api/middleware.js
@@ -5,30 +5,31 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 module.exports = (req, res, next) => {
 
-    // check header or url parameters or post parameters for token
-    const token = req.body['x-access-token'] || req.query['x-access-token'] || req.headers['x-access-token'];
+  // check header or url parameters or post parameters for token
+  const token = req.body['x-access-token'] || req.query['x-access-token']
+                || req.headers['x-access-token'];
 
-    // decode token
-    if (token) {
+  // decode token
+  if (token) {
 
-        // verifies secret and checks exp
-        jwt.verify(token, JWT_SECRET, function (err, decoded) {
-            if (err) {
-                return res.status(403).send({
-                    success: false,
-                    message: 'Failed to authenticate token.'
-                });
-            } else {
-                // if everything is good, save to request for use in other routes
-                req.decoded = decoded;
-                next();
-            }
-        });
-    } else {
-        // if there is no token, return an error
+    // verifies secret and checks exp
+    jwt.verify(token, JWT_SECRET, function(err, decoded) {
+      if (err) {
         return res.status(403).send({
-            success: false,
-            message: 'No token provided.'
+          success: false,
+          message: 'Failed to authenticate token.',
         });
-    }
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    // if there is no token, return an error
+    return res.status(403).send({
+      success: false,
+      message: 'No token provided.',
+    });
+  }
 };
