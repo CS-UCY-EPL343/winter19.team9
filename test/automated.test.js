639d7283490156125be1d47b49e297b2b863cc8d
isopho01 <isopho01@cs.ucy.ac.cy>
2020-05-03 15:48:14 +0300

    First automated test

--- test/automated.test.js
+++ test/automated.test.js
@@ -14,39 +14,46 @@ chai.use(chaiAsPromised);
 // global.window = {};
 // window.localStorage = global.localStorage;
 
+// noinspection NodeModulesDependencies,ES6ModulesDependencies
 describe('Navigate to Pages', function() {
   this.timeout(30000);
 
+  // noinspection JSUnresolvedFunction
   before(function(done) {
     driver
         .navigate()
         .to('http://localhost:3000/')
         .then(() => done(), done);
   });
-
+  // noinspection JSUnresolvedFunction
   it('Navigate to Classes', function(done) {
+    // noinspection JSUnresolvedFunction
     driver.wait(until.elementLocated(By.css('a[href="/classes"]')))
         .click()
         .then(() => done(), done);
   });
-
+  // noinspection JSUnresolvedFunction
   it('Navigate to About Us', function(done) {
+    // noinspection JSUnresolvedFunction
     driver.wait(until.elementLocated(By.css('a[href="/about"]')))
         .click()
         .then(() => done(), done);
   });
-
+  // noinspection JSUnresolvedFunction
   it('Navigate to Home Page', function(done) {
+    // noinspection JSUnresolvedFunction
     driver.wait(until.elementLocated(By.css('a[href="/"]')))
         .click()
         .then(() => done(), done);
   });
 });
-
+// noinspection NodeModulesDependencies,ES6ModulesDependencies
 describe('Login', function() {
   this.timeout(30000);
   context('Login Modal', function() {
+    // noinspection JSUnresolvedFunction
     it('open login modal', function(done) {
+      // noinspection JSUnresolvedFunction
       driver
           .wait(until.elementLocated(By.className('login')))
           .click()
@@ -55,6 +62,7 @@ describe('Login', function() {
   });
 
   context('Unsuccessful Login', function() {
+    // noinspection JSUnresolvedFunction
     it('Wrong Username ', function(done) {
       driver.findElement(By.css('input[name="username"]'))
           .sendKeys('aloizo3')
@@ -62,6 +70,7 @@ describe('Login', function() {
               .sendKeys('1234')
               .then(() => driver.findElement(By.css('#login button')).click()
                   .then(() => {
+                    // noinspection JSUnresolvedFunction
                     driver.wait(
                         until.elementLocated(By.className('swal2-content')),
                         7000)
@@ -73,7 +82,7 @@ describe('Login', function() {
                   }),
               ));
     });
-
+    // noinspection JSUnresolvedFunction
     it('Wrong Password', function(done) {
       driver.findElement(By.css('input[name="username"]'))
           .sendKeys('aloizo03')
@@ -81,6 +90,7 @@ describe('Login', function() {
               .sendKeys('124')
               .then(() => driver.findElement(By.css('#login button')).click()
                   .then(() => {
+                    // noinspection JSUnresolvedFunction
                     driver.wait(
                         until.elementLocated(By.className('swal2-content')),
                         7000)
@@ -92,7 +102,7 @@ describe('Login', function() {
                   }),
               ));
     });
-
+    // noinspection JSUnresolvedFunction
     it('Both Wrong', function(done) {
       driver.findElement(By.css('input[name="username"]'))
           .sendKeys('aloizo3')
@@ -100,6 +110,7 @@ describe('Login', function() {
               .sendKeys('124')
               .then(() => driver.findElement(By.css('#login button')).click()
                   .then(() => {
+                    // noinspection JSUnresolvedFunction
                     driver.wait(
                         until.elementLocated(By.className('swal2-content')),
                         7000)
@@ -111,7 +122,7 @@ describe('Login', function() {
                   }),
               ));
     });
-
+    // noinspection JSUnresolvedFunction
     afterEach(function(done) {
       driver.findElement(By.css('input[name="username"]'))
           .clear()
@@ -123,6 +134,7 @@ describe('Login', function() {
   });
 
   context('Successful Login', function() {
+    // noinspection JSUnresolvedFunction
     it('Successful Login', function(done) {
       driver.findElement(By.css('input[name="username"]'))
           .sendKeys('aloizo03')
@@ -130,6 +142,7 @@ describe('Login', function() {
               .sendKeys('1234')
               .then(() => driver.findElement(By.css('#login button')).click()
                   .then(() => {
+                    // noinspection JSUnresolvedFunction
                     driver.wait(
                         until.elementLocated(By.css('a[href="/user/profile"]')),
                         7000);
@@ -138,11 +151,13 @@ describe('Login', function() {
     });
   });
 });
-
+// noinspection NodeModulesDependencies,ES6ModulesDependencies
 describe('Profile', function() {
   this.timeout(30000);
   context('Navigate', function() {
+    // noinspection JSUnresolvedFunction
     it('Navigate to Profile', function(done) {
+      // noinspection JSUnresolvedFunction
       driver.wait(until.elementLocated(By.css('a[href="/user/profile"]')))
           .click()
           .then(() => done(), done);
@@ -150,27 +165,30 @@ describe('Profile', function() {
   });
 
   context('Open Settings', function() {
+    // noinspection NodeModulesDependencies,ES6ModulesDependencies
     describe('Messages', function() {
       const newMessage = {
         title  : 'I am a test',
         message: 'This is a test message',
       };
-
+      // noinspection JSUnresolvedFunction
       it('View Messages', function(done) {
         driver.findElement(By.css('#leftBlock > ul > li:nth-child(1) > button'))
             .click()
             .then(() => done(), done);
       });
-
+      // noinspection JSUnresolvedFunction
       it('Open Send Message Modal', function(done) {
         // Create and submit new message
+        // noinspection JSUnresolvedFunction
         driver.wait(until.elementLocated(By.css('#MessagesModal > button')),
             10000)
             .click()
             .then(() => done(), done);
       });
-
+      // noinspection JSUnresolvedFunction
       it('Select recipient', function(done) {
+        // noinspection JSUnresolvedFunction
         driver.wait(until.elementLocated(By.id('ann-modal-to')), 7000)
             .click()
             .then(() => driver.wait(until.elementLocated(
@@ -178,25 +196,29 @@ describe('Profile', function() {
                 .click())
             .then(() => done(), done);
       });
-
+      // noinspection JSUnresolvedFunction
       it('Write title', function(done) {
+        // noinspection JSUnresolvedFunction
         driver.wait(
             until.elementLocated(By.id('ann-modal-title')), 7000)
             .sendKeys(newMessage.title)
             .then(() => done(), done);
       });
-
+      // noinspection JSUnresolvedFunction
       it('Write Message', function(done) {
+        // noinspection JSUnresolvedFunction
         driver.wait(
             until.elementLocated(By.id('ann-modal-message')), 7000)
             .sendKeys(newMessage.message)
             .then(() => done(), done);
       });
-
+      // noinspection JSUnresolvedFunction
       it('Submit Message', function(done) {
+        // noinspection JSUnresolvedFunction
         driver.wait(until.elementLocated(
             By.css('div.modal-footer > button:nth-child(2)')), 5000).click()
             .then(() => {
+              // noinspection JSUnresolvedFunction
               driver.wait(
                   until.elementLocated(By.className('swal2-content')), 7000)
                   .then(() => driver.wait(
@@ -205,7 +227,7 @@ describe('Profile', function() {
                   .then(() => done(), done);
             });
       });
-
+      // noinspection JSUnresolvedFunction
       it('Check for Message in Website', function(done) {
         // Check if new message is shown in web page
         wait(5000);
@@ -282,8 +304,9 @@ describe('Profile', function() {
       // title  : currentMessages[0].Title, //       message:
       // currentMessages[0].Message, //     }); });
     });
-
+  // noinspection NodeModulesDependencies,ES6ModulesDependencies
     describe.skip('Membership', function() {
+      // noinspection JSUnresolvedFunction
       it('View Membership', function(done) {
         driver.findElement(
             By.css('#leftBlock > ul > li:nth-child(2) > button'))
@@ -291,8 +314,9 @@ describe('Profile', function() {
             .then(() => done(), done);
       });
     });
-
+    // noinspection NodeModulesDependencies,ES6ModulesDependencies
     describe.skip('Events', function() {
+      // noinspection JSUnresolvedFunction
       it('View Events', function(done) {
         driver.findElement(
             By.css('#leftBlock > ul > li:nth-child(3) > button'))
@@ -300,8 +324,9 @@ describe('Profile', function() {
             .then(() => done(), done);
       });
     });
-
+    // noinspection NodeModulesDependencies,ES6ModulesDependencies
     describe.skip('Announcements', function() {
+      // noinspection JSUnresolvedFunction
       it('View Announcements', function(done) {
         driver.findElement(
             By.css('#leftBlock > ul > li:nth-child(4) > button'))
@@ -309,8 +334,9 @@ describe('Profile', function() {
             .then(() => done(), done);
       });
     });
-
+    // noinspection NodeModulesDependencies,ES6ModulesDependencies
     describe.skip('Edit Account', function() {
+      // noinspection JSUnresolvedFunction
       it('View Edit Account', function(done) {
         driver.findElement(
             By.css('#leftBlock > ul > li:nth-child(5) > button'))
@@ -318,8 +344,9 @@ describe('Profile', function() {
             .then(() => done(), done);
       });
     });
-
+    // noinspection JSUnresolvedFunction
     after(function(done) {
+      // noinspection JSUnresolvedFunction
       driver.wait(until.elementLocated(By.css('div.modal-footer > button')),
           5000)
           .sendKeys(Key.ESCAPE)
@@ -330,16 +357,17 @@ describe('Profile', function() {
     });
   });
 });
-
+// noinspection NodeModulesDependencies,ES6ModulesDependencies
 describe('Logout', function() {
-
+  // noinspection JSUnresolvedFunction
   it('Logout', function(done) {
+    // noinspection JSUnresolvedFunction
     driver
         .wait(until.elementLocated(By.className('logout')))
         .click()
         .then(() => done(), done);
   });
-
+  // noinspection JSUnresolvedFunction
   after(function(done) {
     // baseURL = null;
     // driver.close();
