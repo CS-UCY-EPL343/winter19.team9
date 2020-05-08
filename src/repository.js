c8a92ed4fba31de0f4bc9ffebe8d4cae564b42b5
Andreas Paraskeva <33749619+aparas01@users.noreply.github.com>
2020-05-08 23:33:01 +0300

    Update repository.js

--- src/repository.js
+++ src/repository.js
@@ -1,9 +1,6 @@
 import axios from 'axios';
 
-// const BASE_URL = 'http://localhost:5000';
-const BASE_URL = 'https://europe-west1-ffnbackend-d46c6.cloudfunctions.net/app';
-
-// const BASE_URL = 'http://10.16.22.2:5000';  // For Android use your IPv4
+const BASE_URL = 'http://localhost:5000';
 
 export function logIn(data) {
   return axios.post(`${ BASE_URL }/api/auth`, {
