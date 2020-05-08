f225e99b0dc28b9b7da4e59cd61abb3d81c30f57
isopho01 <isopho01@cs.ucy.ac.cy>
2020-05-08 23:56:19 +0300

    Final touches

--- api/config.js
+++ api/config.js
@@ -1,9 +1,1 @@
-module.exports = {
-  // USER: "d6026286e78fae",
-  // PASS: "eb1b245ce410da",
-  // EMAIL: 'test@testing.com'
-  // USER: "apikey",
-  USER: "fitnessfactorynicosia@outlook.com",
-  PASS: "MAqu7hez",
-  EMAIL: 'fitnessfactorynicosia@outlook.com'
-};
+module.exports = {};
